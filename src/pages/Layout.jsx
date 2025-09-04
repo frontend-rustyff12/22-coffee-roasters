db34c3cede93b38cdf1e50fa7080f10905685733
Russell <75209707+rustyff12@users.noreply.github.com>
2025-09-04 13:00:48 +0200

    Error page

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
+
+      <Outlet />
+
+      {/* <Footer /> */}
     </>
   );
 }
