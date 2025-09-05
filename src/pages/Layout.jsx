70f4b71ce3432a39687835a63d7e5211bd223716
Russell <75209707+rustyff12@users.noreply.github.com>
2025-09-05 15:34:48 +0200

    Footer layout

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
-// import Footer from "../components/Footer";
+import Footer from "../components/Footer";
 
 export default function Layout() {
   return (
     <>
       <Header />
-
-      <Outlet />
-
-      {/* <Footer /> */}
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </>
   );
 }
