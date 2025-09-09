ed010021317f77dbcd325992a40a0f431b226450
Russell <75209707+rustyff12@users.noreply.github.com>
2025-09-09 11:29:45 +0200

    About commitment mobile

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,10 @@
 import AboutHero from "../components/about/AboutHero";
+import Commitment from "../components/about/Commitment";
 export default function About() {
   return (
     <>
       <AboutHero />
+      <Commitment />
     </>
   );
 }
